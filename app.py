ddc38f9d7a966cac36d4b8db45c7cc0aa8037e5b
veerwalia11 <veerwalia27@newmanschool.org>
2026-02-22 22:52:51 -0600

    Reduce memory for Render

--- app.py
+++ app.py
@@ -332,4 +332,6 @@ def preds_file():
 # Main
 # -------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
